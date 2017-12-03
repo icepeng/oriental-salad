4ba5eec1ca89e1c9b9be38153834d6cde0d2dd77
ha <typ0@naver.com>
2017-12-04 02:50:44 +0900

    fix: remove unused submitAttempt, goto next card when submit

--- src/app/judge-card/judge-card-form/judge-card-form.component.ts
+++ src/app/judge-card/judge-card-form/judge-card-form.component.ts
@@ -18,7 +18,6 @@ export class JudgeCardFormComponent implements OnInit, OnDestroy {
   cardPrev: Card | null;
   cardNext: Card | null;
   formGroup: FormGroup;
-  submitAttempt: boolean;
   unsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
@@ -41,7 +40,6 @@ export class JudgeCardFormComponent implements OnInit, OnDestroy {
       ]),
       description: new FormControl('')
     });
-    this.submitAttempt = false;
 
     Observable.combineLatest(
       this.judgeCardService.cardListFiltered,
@@ -68,14 +66,15 @@ export class JudgeCardFormComponent implements OnInit, OnDestroy {
 
   submit(value: Judge, code: string) {
     if (this.formGroup.invalid) {
-      this.submitAttempt = true;
       return;
     }
     this.judgeCardService.saveJudge(value, code);
     if (!this.cardNext) {
       return this.router.navigate(['/', 'judge']);
     }
-    this.submitAttempt = false;
+    this.router.navigate(['../', this.cardNext.code], {
+      relativeTo: this.route,
+    });
   }
 
   prev() {
@@ -85,7 +84,6 @@ export class JudgeCardFormComponent implements OnInit, OnDestroy {
     this.router.navigate(['../', this.cardPrev.code], {
       relativeTo: this.route
     });
-    this.submitAttempt = false;
   }
 
   next() {
@@ -95,7 +93,6 @@ export class JudgeCardFormComponent implements OnInit, OnDestroy {
     this.router.navigate(['../', this.cardNext.code], {
       relativeTo: this.route
     });
-    this.submitAttempt = false;
   }
 
   canDeactivate() {
