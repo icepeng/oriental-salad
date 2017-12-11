0a57fc36a15dc386a10fe80bc1c712317d5b52c6
ha <typ0@naver.com>
2017-12-11 22:43:26 +0900

    fix: reload on route change which not destroy fb-comment component

--- src/app/shared/fb-comments/fb-comments.component.ts
+++ src/app/shared/fb-comments/fb-comments.component.ts
@@ -1,18 +1,36 @@
-import { Component, Input, AfterViewInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Component, AfterViewInit, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-fb-comments',
   templateUrl: './fb-comments.component.html',
   styleUrls: ['./fb-comments.component.scss'],
 })
-export class FbCommentsComponent implements AfterViewInit {
-  @Input() href = window.location.href;
+export class FbCommentsComponent implements OnInit, OnDestroy, AfterViewInit {
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.subscription = this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(() => this.reloadComment());
+  }
 
   ngAfterViewInit() {
+    this.reloadComment();
+  }
+
+  reloadComment() {
     const lb = document.getElementById('fbc');
-    lb.innerHTML = `<div class="fb-comments" data-width="100%" data-href="${this.href} data-numposts="5"" ></div>`;
+    lb.innerHTML = `<div class="fb-comments" data-width="100%" data-href="${
+      window.location.href
+    } data-numposts="5"" ></div>`;
     (window as any).FB.XFBML.parse(lb);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
