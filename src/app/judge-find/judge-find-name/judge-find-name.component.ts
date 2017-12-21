8b0fcecadb3c7612c10ba1900eeb0e82f6cf8f14
ha <typ0@naver.com>
2017-12-21 22:25:48 +0900

    feat: judge-find-name datagrid

--- src/app/judge-find/judge-find-name/judge-find-name.component.ts
+++ src/app/judge-find/judge-find-name/judge-find-name.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Comparator, SortOrder } from 'clarity-angular';
+import { Comparator, DatagridFilter, Filter, SortOrder } from 'clarity-angular';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { JudgeFindNameService } from './judge-find-name.service';
 import { SearchResult } from './searchResult';
@@ -19,6 +20,34 @@ class RankComparator implements Comparator<SearchResult> {
   }
 }
 
+class CountComparator implements Comparator<SearchResult> {
+  compare(a: SearchResult, b: SearchResult) {
+    return a.judgeCount - b.judgeCount;
+  }
+}
+
+class RankFilter implements Filter<SearchResult> {
+  checked = true;
+  changes = new Subject<any>();
+
+  constructor() {
+    this.changes.next(true);
+  }
+
+  toggle() {
+    this.checked = !this.checked;
+    this.changes.next(true);
+  }
+
+  isActive(): boolean {
+    return true;
+  }
+
+  accepts(searchResult: SearchResult) {
+    return this.checked ? !!searchResult.rank : true;
+  }
+}
+
 @Component({
   selector: 'app-judge-find-name',
   templateUrl: './judge-find-name.component.html',
@@ -31,6 +60,8 @@ export class JudgeFindNameComponent implements OnInit {
   rankOrder = SortOrder.Asc;
   scoreComparator = new ScoreComparator();
   rankComparator = new RankComparator();
+  countComparator = new CountComparator();
+  rankFilter = new RankFilter();
 
   constructor(
     private judgeFindNameService: JudgeFindNameService,
